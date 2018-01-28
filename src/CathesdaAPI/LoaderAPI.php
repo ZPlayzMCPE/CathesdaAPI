badf8297052fdfc37fa71890f434b0ada3594360
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-28 11:48:36 +0100

    Update LoaderAPI.php

--- src/CathesdaAPI/LoaderAPI.php
+++ src/CathesdaAPI/LoaderAPI.php
@@ -6,10 +6,19 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 
+# CATHESDAAPI COMMANDS #
+use Commands\Hub;
+
 class LoaderAPI extends PluginBase implements Listener {
 
     public function onEnable(){
       $this->getLogger()->info("Loading CathesdaAPI");
+        $this->RegCommands();
      }
+    
+    public function RegCommands(){
+        $this->getServer()->getCommandMap()->register("Hub", new HubCommand("Hub", $this));
+        
     }
    
+}
