19fbee1a7b489544f505a1f55f0cdbdbe30f43b6
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-28 11:35:05 +0100

    Create Cathesda.php

---
+++ src/Cathesda/Cathesda.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+* ╔═══╗╔═══╗╔════╗╔╗─╔╗╔═══╗╔═══╗╔═══╗╔═══╗
+* ║╔═╗║║╔═╗║║╔╗╔╗║║║─║║║╔══╝║╔═╗║╚╗╔╗║║╔═╗║
+* ║║─╚╝║║─║║╚╝║║╚╝║╚═╝║║╚══╗║╚══╗─║║║║║║─║║
+* ║║─╔╗║╚═╝║──║║──║╔═╗║║╔══╝╚══╗║─║║║║║╚═╝║
+* ║╚═╝║║╔═╗║──║║──║║─║║║╚══╗║╚═╝║╔╝╚╝║║╔═╗║
+* ╚═══╝╚╝─╚╝──╚╝──╚╝─╚╝╚═══╝╚═══╝╚═══╝╚╝─╚╝
+ * Cathesda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Cathesda Team
+ * @link https://www.cathesda.de/
+ *
+ *
+*/
+
+namespace Cathesda;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\{Player, Server};
+use pocketmine\utils\TextFormat as Cathesda;
+
+#------Cathesda Events------#
+
+class Cathesda extends PluginBase {
+
+  const PREFIX = "§bA§eP§cI §7|§r ";
+  
+  public function onEnable(){
+    $this->getLogger()->info(self::PREFIX . Cathesda::GREEN . "CathesdaAPI Loaded");
+    
+    }
+    
+    
+    #------Cathesda Events------#
+    
+    
+    
+   }
